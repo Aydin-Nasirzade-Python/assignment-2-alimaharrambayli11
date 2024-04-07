da9cbc4aede9d14938ae8d0c9bba0385b7784a21
alimaharrambayli11 <165707506+alimaharrambayli11@users.noreply.github.com>
2024-04-07 14:44:30 +0400

    Update hw2task5_test.py

--- hw2task5_test.py
+++ hw2task5_test.py
@@ -1,114 +1,72 @@
-import unittest
-from io import StringIO
-from unittest.mock import patch
-import Task_5
+def main():
+  
+  m=input("Enter a month [ex. March]: ")
+  d=int(input("Enter the day [ex. 12]: "))
+    
+  if d>31 or d<0 or m not in ["December","January","February","March","April","May","June","July","August","September","October","November"]or (d>30 and m in ["April","June","September","November"])or(d>29 and m=="February"):
+        print("Either a month or a day is invalid!")
+  else:
+      if m=="December" or m=="January":
+          if (m=="December" and d<22)or(m=="January" and d>19):
+              pass
+          else:
+              print("Your zodiac sign is Capricorn")
+      if m=="January" or m=="February":
+          if (m=="January" and d<20)or(m=="February" and d>18):
+              pass
+          else:
+              print("Your zodiac sign is Aquarius")
+      if m=="February" or m=="March":
+          if (m=="February" and d<19)or(m=="March" and d>20):
+              pass
+          else:
+              print("Your zodiac sign is Pisces")
+      if m=="March" or m=="April":
+          if (m=="March" and d<21)or(m=="April" and d>19):
+              pass
+          else:
+              print("Your zodiac sign is Aries")
+      if m=="April" or m=="May":
+          if (m=="April" and d<20)or(m=="May" and d>20):
+              pass
+          else:
+              print("Your zodiac sign is Taurus")
+      if m=="May" or m=="June":
+          if (m=="May" and d<21)or(m=="June" and d>20):
+              pass
+          else:
+              print("Your zodiac sign is Gemini")
+      if m=="June" or m=="July":
+          if (m=="June" and d<21)or(m=="July" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Cancer")
+      if m=="July" or m=="August":
+          if (m=="July" and d<23)or(m=="August" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Leo")
+      if m=="August" or m=="September":
+          if (m=="August" and d<23)or(m=="September" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Virgo")
+      if m=="September" or m=="October":
+          if (m=="September" and d<23)or(m=="October" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Libra")
+      if m=="October" or m=="November":
+          if (m=="October" and d<23)or(m=="November" and d>21):
+              pass
+          else:
+              print("Your zodiac sign is Scorpion")
+      if m=="November" or m=="December":
+          if (m=="November" and d<22)or(m=="December" and d>21):
+              pass
+          else:
+              print("Your zodiac sign is Sagittarius")
+  pass
 
-class TestMainFunction(unittest.TestCase):
-
-    @patch('builtins.input', side_effect=["March", "12"])
-    def test_main_input_calls(self, mock_input):
-        Task_5.main() 
-        expected_calls = ["Enter a month [ex. March]: ", "Enter the day [ex. 12]: "]
-        # Check if input was called with the expected arguments
-        self.assertEqual(mock_input.call_args_list, [unittest.mock.call(arg) for arg in expected_calls])
-
-    def test_zodiac1(self):
-        test_cases = [
-            {'input': ["December", 22], 'expected_output': 'Your zodiac sign is Capricorn'},
-            {'input': ["December", 31], 'expected_output': 'Your zodiac sign is Capricorn'},
-            {'input': ["January", 1], 'expected_output': 'Your zodiac sign is Capricorn'},
-            {'input': ["January", 19], 'expected_output': 'Your zodiac sign is Capricorn'},
-            {'input': ["January", 20], 'expected_output': 'Your zodiac sign is Aquarius'},
-            {'input': ["January", 31], 'expected_output': 'Your zodiac sign is Aquarius'},
-            {'input': ["February", 1], 'expected_output': 'Your zodiac sign is Aquarius'},
-            {'input': ["February", 18], 'expected_output': 'Your zodiac sign is Aquarius'},
-            {'input': ["February", 19], 'expected_output': 'Your zodiac sign is Pisces'},
-            {'input': ["February", 29], 'expected_output': 'Your zodiac sign is Pisces'},
-            {'input': ["March", 1], 'expected_output': 'Your zodiac sign is Pisces'},
-            {'input': ["March", 20], 'expected_output': 'Your zodiac sign is Pisces'}
-        ]
-        for test_case in test_cases:
-            with patch('builtins.input', side_effect=test_case['input']), \
-                 patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                Task_5.main()
-            self.assertEqual(mock_stdout.getvalue().strip(), test_case['expected_output'])
-
-    def test_zodiac2(self):
-        test_cases = [
-            {'input': ["June", 20], 'expected_output': 'Your zodiac sign is Gemini'},
-            {'input': ["June", 1], 'expected_output': 'Your zodiac sign is Gemini'},
-            {'input': ["May", 31], 'expected_output': 'Your zodiac sign is Gemini'},
-            {'input': ["May", 21], 'expected_output': 'Your zodiac sign is Gemini'},
-            {'input': ["May", 20], 'expected_output': 'Your zodiac sign is Taurus'},
-            {'input': ["May", 1], 'expected_output': 'Your zodiac sign is Taurus'},
-            {'input': ["April", 30], 'expected_output': 'Your zodiac sign is Taurus'},
-            {'input': ["April", 20], 'expected_output': 'Your zodiac sign is Taurus'},
-            {'input': ["April", 19], 'expected_output': 'Your zodiac sign is Aries'},
-            {'input': ["April", 1], 'expected_output': 'Your zodiac sign is Aries'},
-            {'input': ["March", 31], 'expected_output': 'Your zodiac sign is Aries'},
-            {'input': ["March", 21], 'expected_output': 'Your zodiac sign is Aries'}
-        ]
-        for test_case in test_cases:
-            with patch('builtins.input', side_effect=test_case['input']), \
-                 patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                Task_5.main()
-            self.assertEqual(mock_stdout.getvalue().strip(), test_case['expected_output'])
-
-    def test_zodiac3(self):
-        test_cases = [
-            {'input': ["June", 21], 'expected_output': 'Your zodiac sign is Cancer'},
-            {'input': ["June", 30], 'expected_output': 'Your zodiac sign is Cancer'},
-            {'input': ["July", 1], 'expected_output': 'Your zodiac sign is Cancer'},
-            {'input': ["July", 22], 'expected_output': 'Your zodiac sign is Cancer'},
-            {'input': ["July", 23], 'expected_output': 'Your zodiac sign is Leo'},
-            {'input': ["July", 31], 'expected_output': 'Your zodiac sign is Leo'},
-            {'input': ["August", 1], 'expected_output': 'Your zodiac sign is Leo'},
-            {'input': ["August", 22], 'expected_output': 'Your zodiac sign is Leo'},
-            {'input': ["August", 23], 'expected_output': 'Your zodiac sign is Virgo'},
-            {'input': ["August", 31], 'expected_output': 'Your zodiac sign is Virgo'},
-            {'input': ["September", 1], 'expected_output': 'Your zodiac sign is Virgo'},
-            {'input': ["September", 22], 'expected_output': 'Your zodiac sign is Virgo'}
-        ]
-        for test_case in test_cases:
-            with patch('builtins.input', side_effect=test_case['input']), \
-                 patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                Task_5.main()
-            self.assertEqual(mock_stdout.getvalue().strip(), test_case['expected_output'])
-
-    def test_zodiac4(self):
-        test_cases = [
-            {'input': ["December", 21], 'expected_output': 'Your zodiac sign is Sagittarius'},
-            {'input': ["December", 1], 'expected_output': 'Your zodiac sign is Sagittarius'},
-            {'input': ["November", 30], 'expected_output': 'Your zodiac sign is Sagittarius'},
-            {'input': ["November", 22], 'expected_output': 'Your zodiac sign is Sagittarius'},
-            {'input': ["November", 21], 'expected_output': 'Your zodiac sign is Scorpion'},
-            {'input': ["November", 1], 'expected_output': 'Your zodiac sign is Scorpion'},
-            {'input': ["October", 31], 'expected_output': 'Your zodiac sign is Scorpion'},
-            {'input': ["October", 23], 'expected_output': 'Your zodiac sign is Scorpion'},
-            {'input': ["October", 22], 'expected_output': 'Your zodiac sign is Libra'},
-            {'input': ["October", 1], 'expected_output': 'Your zodiac sign is Libra'},
-            {'input': ["September", 30], 'expected_output': 'Your zodiac sign is Libra'},
-            {'input': ["September", 23], 'expected_output': 'Your zodiac sign is Libra'}
-        ]
-        for test_case in test_cases:
-            with patch('builtins.input', side_effect=test_case['input']), \
-                 patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                Task_5.main()
-            self.assertEqual(mock_stdout.getvalue().strip(), test_case['expected_output'])
-
-    def test_invalid_date(self):
-        test_cases = [
-            {'input': ["February", 30], 'expected_output': 'Either a month or a day is invalid!'},
-            {'input': ["June", 31], 'expected_output': 'Either a month or a day is invalid!'},
-            {'input': ["November", 31], 'expected_output': 'Either a month or a day is invalid!'},
-            {'input': ["May", -5], 'expected_output': 'Either a month or a day is invalid!'},
-            {'input': ["july", 31], 'expected_output': 'Either a month or a day is invalid!'},
-        ]
-        for test_case in test_cases:
-            with patch('builtins.input', side_effect=test_case['input']), \
-                 patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                Task_5.main()
-            self.assertEqual(mock_stdout.getvalue().strip(), test_case['expected_output'])
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+  main()
